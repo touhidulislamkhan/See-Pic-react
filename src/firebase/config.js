fe3194b41a9f9a1668675a21b80b1f57b49f3ae5
touhidulislamkhan <touhidikhan0@gmail.com>
2021-05-02 03:28:29 +0600

    firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,11 +5,11 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.Firebase_AUTH_DOMAIN,
-    projectId: process.env.Firebase_PROJECT_ID,
-    storageBucket: process.env.Firebase_STORAGE_BUCKET,
-    messagingSenderId: process.env.Firebase_MESSAGING_SENDER_ID,
-    appId: process.env.Fireabse_APP_ID
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
